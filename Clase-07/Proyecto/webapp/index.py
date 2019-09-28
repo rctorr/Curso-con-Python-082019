e38baa418ce4ba6d3ac40ed944fded6603cabfcb
Ricardo Torres <rictor@cuhrt.com>
2019-09-28 11:38:32 -0500

    Actualizando archivo index.py en Proyecto de Clase-07

--- Clase-07/Proyecto/webapp/index.py
+++ Clase-07/Proyecto/webapp/index.py
@@ -7,12 +7,29 @@ HOST = "localhost"
 PORT = 8000
 
 # Agregar ruta y vista para atender peticiones de archivos estáticos
+@route('/static/<path:path>')
+def callback(path):
+    return static_file(path, root='./static')
+
+def leer_html(nomarch):
+
+    with open(nomarch) as da:
+        contenido = da.read()
+
+    return contenido
 
 # Agregar ruta y vista para atender petición raíz /
+@route("/")
+def index():
+    plantilla = leer_html("formulario.html")
+    return plantilla
 
 # Agregar ruta y vista para atender petición raíz / con datos POST
-
+@route("/post")
+def post():
+    pass
 
 if __name__ == "__main__":
     # Inicializa el servidor de la aplicación web
     run(host=HOST, port=PORT, debug=True, reloader=True)
+
